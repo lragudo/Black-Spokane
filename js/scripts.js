45ddafbe93c29158ad416594ff698e59a185a8c1
Lillian <59583025+lragudo@users.noreply.github.com>
2020-07-28 15:17:42 -0700

    Added JS code from codepen
    Added code from sorting
    https://codepen.io/collection/XWYzVg

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+/* 
+- INITIALIZE ISOTOPE
+- Arguements: ( Parent Container , OPTIONS )
+*/ 
+const iso = new Isotope( '.filter-container', {
+    itemSelector: '.content',
+    layoutMode: 'fitRows'
+  });
+  
+  /* 
+  - CLICK BUTTON, FILTER CONTENT
+  */
+  
+  // cache filters 
+  const filters = document.querySelector('.filters');
+  
+  // click a filter, then filter content-children
+  filters.addEventListener( 'click', function( event ) {
+  
+    // not `const` because this value changes. use `let`   
+    let filterValue = event.target.getAttribute('data-filter');
+    // check-check
+    console.log(filterValue); 
+  
+    // Isotope re-arrange content by filtered value
+    iso.arrange({ filter: filterValue });
+  
+  });
+  
+  
